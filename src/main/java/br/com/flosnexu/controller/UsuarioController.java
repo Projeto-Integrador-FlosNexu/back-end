8759fe8dd3d87060a34a6a283485996741cda2de
Gabriel Da Costa Lima <63054618+costalima10@users.noreply.github.com>
2024-03-14 16:28:41 -0300

    Update UsuarioController.java resolvendo imports

--- src/main/java/br/com/flosnexu/controller/UsuarioController.java
+++ src/main/java/br/com/flosnexu/controller/UsuarioController.java
@@ -3,6 +3,9 @@ package br.com.flosnexu.controller;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.web.server.ResponseStatusException;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
